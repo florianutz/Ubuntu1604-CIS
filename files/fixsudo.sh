1e14b5ecd66d53f0dbda445cc549fd297f5fbfc3
Florian Utz <florian.utz@github.com>
2018-03-22 21:45:46 +0100

    add debug option for troubleshooting #8

--- files/fixsudo.sh
+++ files/fixsudo.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-sudopath=($(grep secure_path /etc/sudoers | cut -f2 -d= |cut -f2 -d\"|tr ":" " "))
-for i in ${sudopath[@]}
+sudopath=($(grep secure_path /etc/sudoers | cut -f2 -d= |cut -f2 -d\"))
+IFS=:
+for i in ${sudopath[*]}
 do
   if [ -d "$i" ]
    then newsudopath+=($i)
   fi
 done
-echo $(IFS=:; echo "${newsudopath[*]}")
+echo "${newsudopath[*]}"
+
+if [ "$1" == "debug" ]
+  then
+    echo
+    echo "Debug secure_path: $(grep secure_path /etc/sudoers)"
+    echo "Debug sudopath: $sudopath"
+    echo "Debug result: ${newsudopath[*]}"
+fi
